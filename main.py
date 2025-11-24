106c9f34d405c9f8bc61f9be4233bde216ee4622
giadv <gia1211dv@gmail.com>
2025-11-24 12:57:41 +0100

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+import adafruit_dht
+import board
+
+dht = adafruit_dht.DHT11(board.D4)
+
+try:
+    temperature_c = dht.temperature
+    humidity = dht.humidity
+    print(f"Temp: {temperature_c} C  Humidity: {humidity}%")
+except RuntimeError as e:
+    print(f"Error reading from DHT11: {e}")
+    
